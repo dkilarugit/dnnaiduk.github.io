f6d2c0475608fe58fa74219491f5020cba832ae6
dkilarugit <dkilaru@umassd.edu>
2025-06-23 18:31:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Thanks for contacting me! I’ll get back to you soon.");
+  this.reset();
+});
